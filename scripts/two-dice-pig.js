64b0ddd1ca019d5eb5313e4a44de8525c982f847
Mukul Kr Jha <jimickeyji@gmail.com>
2019-06-02 22:23:32 +0530

    Extend the Logic of Game for Two dices

--- scripts/two-dice-pig.js
+++ scripts/two-dice-pig.js
@@ -58,26 +58,37 @@ document.querySelector('.roll-dice').addEventListener('click',function(){
  if(isPlaying){
     //1.generate random number 
     console.log("isPlaying is"+isPlaying);
-    var dice=Math.floor(Math.random()*6)+1;
-    console.log(dice);
+    var dice1=Math.floor(Math.random()*6)+1;
+    var dice2=Math.floor(Math.random()*6)+1;
+    console.log("dice1 : " + dice1);
+    console.log("dice2 : " + dice2);
     //2. image of dice
     document.querySelector('.dice-img').style.display="block";
-    document.querySelector('.dice-img').src="./images/dice-"+dice+".png";
+    document.querySelector('.dice-img-1').src="./images/dice-"+dice1+".png";
+    document.querySelector('.dice-img-2').src="./images/dice-"+dice2+".png";
     //3. update the score
-    if(dice!==1)  //checking if dice is not 1 (player changing rule )
+    if(dice1!==1 && dice2!==1)  //checking if dice is not 1 (player changing rule )
     {
-         total+=dice;
+         total+=dice1+dice2;
          if(total+scores[ activePlayer]>=100)
          {
-             winnerFunction();
+             winnerFunction(total+scores[ activePlayer]);
          }
          document.querySelector('.current-score--'+activePlayer).textContent=total;
     }
+    else if(dice1!==1 || dice2!==1)  //checking if dice is not 1 (player changing rule )
+    {
+      document.querySelector('.dice-img').style.display="block";
+      dice1 == 1 ? document.querySelector('.dice-img-1').src="./images/turnChanged"+activePlayer+".png":document.querySelector('.dice-img-2').src="./images/turnChanged"+activePlayer+".png";
+      total=0;
+      nextPlayer();
+    }       
     else 
     {
       document.querySelector('.dice-img').style.display="block";
-      document.querySelector('.dice-img').src="turnChanged"+activePlayer+".png";
+      document.querySelector('.dice-img').src="./images/turnChanged"+activePlayer+".png";
       total=0;
+      scores[activePlayer] =0;
         nextPlayer();
     }
  }
@@ -93,24 +104,26 @@ document.querySelector('.hold').addEventListener('click',function(){
     console.log(scores[activePlayer]);
     if(scores[activePlayer]>=100)
     	{
-        winnerFunction();
+        winnerFunction(scores[activePlayer]);
     	}
    else{
-           document.querySelector('.dice-img').style.display="none";
+           document.querySelector('.dice-img').style.display="block";
+           document.querySelector('.dice-img').src="./images/turnChanged"+activePlayer+".png";
    	       nextPlayer();
        }
     total=0;
   }
 });
-var winnerFunction=function()
+var winnerFunction=function(score)
 {
   
   document.querySelector('.player__name--'+activePlayer).innerHTML="winner!!";
   document.querySelector('.player--'+activePlayer).classList.add('winner');
   console.log("active Player is "+activePlayer);
-  document.querySelector(".current-score--"+activePlayer).textContent=0;  
+  document.querySelector('.player__score-glb--'+activePlayer).textContent=score;
+  // document.querySelector(".current-score--"+activePlayer).textContent=0;  
   document.querySelector('#dot--'+activePlayer).classList.remove('active');
-  document.querySelector('.dice-img').style.display="none";
+  document.querySelector('.dice-img-container').style.display="none";
 
   isPlaying=0;
 
